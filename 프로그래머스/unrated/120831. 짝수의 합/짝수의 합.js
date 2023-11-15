7d125fcec20e70db5f835853e9343cb8eec8c306
pompomko <sdw12334@gmail.com>
2023-11-15 11:47:51 +0900

    [level unrated] Title: 짝수의 합, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    
+  let arr =[];
+  //2로 나누어서 나머지가 0이라면 짝수 
+  for(i =1 ;i<=n;i++){
+      if(i % 2 === 0){ arr.push(i)}
+  }
+
+  return arr.reduce((a,b)=>(a+b),0)
+  //나눈걸 배열안에 모두 집어널고 다 더한다
+  
+}
