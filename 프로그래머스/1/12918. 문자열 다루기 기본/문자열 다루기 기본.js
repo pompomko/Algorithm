bd31ce4185322ea1177b572418a616a081baf351
pompomko <sdw12334@gmail.com>
2023-12-14 09:40:27 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,10 @@
+function solution(s) {
+    // const a = (s)  //연산기호를 넣으면 자바스크립트 특성상 숫자로 변경
+    //문자 타입 구분후 삼항연산자 
+    // const answer = +s >= 21 ? true : false 
+    // const answer = isNaN(s) == true ? false : true
+    // !isNaN(s)
+    return (s.length === 4 || s.length === 6) && s == parseInt(s)
+    
+     
+}
