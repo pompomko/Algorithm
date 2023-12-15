313ed9cdae16e8c6c793891b3347fe1f82e72eca
pompomko <sdw12334@gmail.com>
2023-12-15 18:52:13 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.05 ms, Memory: 67.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,12 @@
+function solution(arr1, arr2) {
+    var answer = [];
+    
+    for(let i=0; i<arr1.length; i++){ //[1,2][2,3] //[3,4][5,6]
+        let sum = [];
+        for(let j=0; j<arr1[i].length; j++){ //arr1 안의 배열의 길이 (2번)
+            sum.push(arr1[i][j] + arr2[i][j]) // 1번째 1+3, 2번째 2+4 / 3번째 2+5 4번째 3+6
+        }
+        answer.push(sum) // [4,6],[7,9]
+    }
+    return answer
+}
